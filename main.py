7f107a419204d42563fcf7823dbbf946023de040
Kane Pereira <kane.pereira18@gmail.com>
2023-08-24 03:50:10 -0400

    count words in book after printing out book text

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 with open('books/frankenstein.txt') as f:
     file_contents = f.read()
     print(file_contents)
+
+    counter = 0
+    words = file_contents.split()
+    for word in words:
+        counter += 1;
+    print(counter)
+    
