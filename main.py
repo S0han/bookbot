f8390f7d0df42c00900c9120a36f9eca83e06556
Kane Pereira <kane.pereira18@gmail.com>
2023-08-24 04:12:12 -0400

    create dictionary for character count

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 with open('books/frankenstein.txt') as f:
     file_contents = f.read()
-    print(file_contents)
+    # print(file_contents)
 
     counter = 0
     words = file_contents.split()
     for word in words:
         counter += 1;
-    print(counter)
+    # print(counter)
+
+    dict = {}
+    string_content = ''.join(words).lower()
+    for chr in string_content:
+        if chr not in dict:
+            dict[chr] = 0
+            dict[chr] += 1
+        else:
+            dict[chr] += 1 
+    print(dict)
     
